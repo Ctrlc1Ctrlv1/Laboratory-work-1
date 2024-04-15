1c5d994a5949f1370a7b193b42b06be31fc210ae
Daniel <daniil.sukach.042@gmail.com>
2024-04-15 16:51:53 +0200

    First release

--- main_1.c
+++ main_1.c
@@ -1,114 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
-int set_max_size(int max_size, int numLetters) {
-// set a new max length of word
-    if (numLetters > max_size) {
-        max_size = numLetters;
-    }
-    return max_size;
+bool is_separator(char ch) {
+    char separators[] = ",=+/?\\{}[]#.';:!&()_>< ";
+    return strchr(separators, ch) != NULL || ch == '\n' || ch == EOF;
 }
 
-bool have_separate(char text_str) {
-// check if symbol is separator
-    char separator[] = ",=+/?\\{}[]#.';:!&()_><";
-    bool have_separator = false;
-        for (int i = 0; i < strlen(separator); i++) {
-            if (separator[i] == text_str) {
-                have_separator = true;
-                break;
-            }
-        }
-    return have_separator;
-}
-
-void replace_separator(const char *filename) {
-// replace all kinds of separators to ' ' and writes changed text to file temp.txt
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Can't open file.\n");
-        exit(1);
+bool process_word(char* word, int* max_size) {
+    int len = strlen(word);
+    if (len > 30) {
+        printf("Text has a word with the length more than 30 letters: %s\nThe program was stopped", word);
+        return false; // Word exceeds maximum length
     }
-
-    char tempName[] = "temp.txt";
-    FILE *tempFile = fopen(tempName, "w");
-    if (tempFile == NULL) {
-        fprintf(stderr, "Can't create temp file.\n");
-        exit(1);
+    if (len > *max_size) {
+        *max_size = len;
     }
-
-    char ch;
-    while ((ch = fgetc(file)) != EOF) {
-        if (have_separate(ch)) {
-            fputc(' ', tempFile);
-        } else {
-            fputc(ch, tempFile);
-        }
-    }
-
-    fclose(file);
-    fclose(tempFile);
-}
-
-void write_new_fin_file(char line[]){
-// delete all words in fin.txt and write a new word
-    FILE *fin_file = fopen("fin.txt", "w");
-    fprintf(fin_file, "%s\n", line);
-    fclose(fin_file);
-}
-
-bool word_already_appended(char line[]){
-// check if the word has been written into the file fin.txt
-    char fin_line[30];
-    FILE *fin_file = fopen("fin.txt", "r+");
-    while (fscanf(fin_file, "%s", fin_line) == 1){
-        if (strcmp(line, fin_line) == 0){
-            fclose(fin_file);
-            return true;}
-    }
-    fclose(fin_file);
-    return false;
-}
-
-void append_to_fin_file(char line[]){
-// append word to file fin.txt
-    FILE *fin_file = fopen("fin.txt", "a");
-    if (!word_already_appended(line)){
-        fprintf(fin_file, "%s\n", line);
-    }
-    fclose(fin_file);
+    return true;
 }
 
 int main() {
-    replace_separator("Test1.txt");
+    system("chcp 65001");
+    setlocale(LC_ALL, "Ukrainian");
 
-    FILE *file = fopen("temp.txt", "r");
+    const char* filename = "Test5.txt";
+    FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
         return EXIT_FAILURE;
     }
 
-    char line[30];
-    int max_size = 1;
-    int old_max_size;
-    int num_of_words = 0;
+    char word[31];
+    int word_len = 0;
+    int max_size = 0;
+    char ch;
 
-    while (fscanf(file, "%s", line) == 1){
-        old_max_size = max_size;
-        int current_word_length = strlen(line);
-        max_size = set_max_size(max_size, current_word_length);
-
-        if(current_word_length > old_max_size){
-            write_new_fin_file(line);
-            num_of_words = 0;}
-        else if(current_word_length == max_size){
-            append_to_fin_file(line);
-            num_of_words += 1;}
+    // First pass: Find the maximum length of the words
+    while ((ch = fgetc(file)) != EOF) {
+        if (is_separator(ch)) {
+            if (word_len > 0) {
+                word[word_len] = '\0';
+                if (!process_word(word, &max_size)) {
+                    fclose(file);
+                    return EXIT_FAILURE;
+                }
+                word_len = 0;
+            }
+        } else {
+            if (word_len < sizeof(word) - 1) {
+                word[word_len++] = ch;
+            }
+        }
     }
+    // Check if the last word needs to be processed
+    if (word_len > 0) {
+        word[word_len] = '\0';
+        process_word(word, &max_size);
+    }
+
     fclose(file);
-    printf("No problem.\n");
-    printf("Length of the longest word: %d", max_size);
+
+    // Reopen the file for the second pass
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error reopening file");
+        return EXIT_FAILURE;
+    }
+
+    FILE *fout = fopen("fin.txt", "w");
+    if (fout == NULL) {
+        perror("Error opening output file");
+        fclose(file);
+        return EXIT_FAILURE;
+    }
+
+    // Second pass: Write all words with the maximum length to the output file
+    word_len = 0;
+    while ((ch = fgetc(file)) != EOF) {
+        if (is_separator(ch)) {
+            if (word_len > 0) {
+                word[word_len] = '\0';
+                if (strlen(word) == max_size) {
+                    fprintf(fout, "%s\n", word);
+                }
+                word_len = 0;
+            }
+        } else {
+            if (word_len < sizeof(word) - 1) {
+                word[word_len++] = ch;
+            }
+        }
+    }
+    // Again, check if the last word needs to be processed
+    if (word_len > 0) {
+        word[word_len] = '\0';
+        if (strlen(word) == max_size) {
+            fprintf(fout, "%s\n", word);
+        }
+    }
+
+    fclose(file);
+    fclose(fout);
+
+    printf("Longest words written to fin.txt\n");
+    printf("Length of the longest word: %d\n", max_size);
 
     return 0;
 }
